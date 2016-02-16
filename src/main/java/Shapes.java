85b9398079c0c4b2f055db0bcc91c6788d2fedda
Younes HAJJI <y.hajji@sii-maroc.com>
2016-02-16 18:08:44 +0100

    initial commit

--- src/main/java/Shapes.java
+++ src/main/java/Shapes.java
@@ -2,13 +2,10 @@ import java.io.StringWriter;
 
 public class Shapes {
 
-	public Shapes(StringWriter out) {
-		// TODO Auto-generated constructor stub
-	}
+    public Shapes(StringWriter out) {
+    }
 
-	public void area(String string, String string2) {
-		// TODO Auto-generated method stub
-		
-	}
+    public void area(String string, String string2) {
+    }
 
 }
