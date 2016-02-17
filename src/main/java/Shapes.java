a6469387ff72951ddd63c0719a766925602f4c93
Younes HAJJI <y.hajji@sii-maroc.com>
2016-02-17 10:27:27 +0100

    version 4.4

--- src/main/java/Shapes.java
+++ src/main/java/Shapes.java
@@ -14,20 +14,20 @@ public class Shapes {
     }
 
     private void createShape(final String string, final String string2) {
-        DimensionExtractor measure = new DimensionExtractor();
-        measure.extract(string2);
+        DimensionExtractor dimensions = new DimensionExtractor();
+        dimensions.extract(string2);
         switch (string) {
         case "SQUARE":
-            shape = new Square(measure.getDimension(0));
+            shape = new Square(dimensions.getDimension(0));
             break;
         case "RECTANGLE":
-            shape = new Rectangle(measure.getDimension(0), measure.getDimension(1));
+            shape = new Rectangle(dimensions.getDimension(0), dimensions.getDimension(1));
             break;
         case "TRIANGLE":
-            if (measure.numberOfDimensions() == 1)
-                shape = new Triangle(measure.getDimension(0));
+            if (dimensions.numberOfDimensions() == 1)
+                shape = new Triangle(dimensions.getDimension(0));
             else
-                shape = new Triangle(measure.getDimension(0), measure.getDimension(1));
+                shape = new Triangle(dimensions.getDimension(0), dimensions.getDimension(1));
             break;
         }
     }
