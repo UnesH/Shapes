2fda99e4d0caab1d0511876fbad9621b44c35b8a
Younes HAJJI <y.hajji@sii-maroc.com>
2016-02-17 12:21:14 +0100

    version 1.7

---
+++ src/main/java/DegradedTriangle.java
@@ -0,0 +1,13 @@
+
+public class DegradedTriangle extends Triangle {
+    protected int base;
+
+    public DegradedTriangle(final int base) {
+        super(base, base);
+        this.base = base;
+    }
+
+    public int area() {
+        return (base * base) / 2;
+    }
+}
